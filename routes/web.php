6c335c8ae55209ddef0a8203c3bccc8c09e350e2
ahsan-eazisols <hannan.shahid@eazisols.com>
2025-11-14 02:50:37 +0500

    Remove the unwanted blade files

--- routes/web.php
+++ routes/web.php
@@ -23,47 +23,21 @@ Route::get('/testing', function () {return view('figmaDesign.testing');});
 
 // home
 Route::get('/testing', function () {return view('home.indexImage');});
-// Route::get('/home-image', function () {return view('home.indexImage');});
-Route::get('/home-video', function () {return view('home.indexVideo');});
 
 // Search routes
 Route::get('/search', [\App\Http\Controllers\SearchController::class, 'search'])->name('search');
-
-// about
-Route::get('/about', function () {return view('about.index');});
-
-// services
-Route::get('/services', function () {return view('services.index');});
-Route::get('/service-detail', function () {return view('services.servicedetail');});
-Route::get('/service-provider', function () {return view('services.serviceprovider');});
-
-// blog
-Route::get('/blogs', function () {return view('blogs.index');});
-Route::get('/blog-single', function () {return view('blogs.singleblog');});
-
-// contact us
-Route::get('/contact-us', function () {return view('contactUs.index');});
 
 // case study
 Route::get('/saloons', function () {return view('caseStudy.index');});
 Route::get('/saloons-single', function () {return view('caseStudy.single');});
 
-// team
-Route::get('/team', function () {return view('ourTeam.index');});
-Route::get('/team-single', function () {return view('ourTeam.single');});
-
-// pricing
-Route::get('/pricing', function () {return view('pricingPlan.index');});
 
 // testimonials
 Route::get('/testimonials', function () {return view('testimonials.index');});
 
 // gallery
-Route::get('/image-gallery', function () {return view('gallery.image');});
 Route::get('/video-gallery', function () {return view('gallery.video');});
 
-// faqs
-Route::get('/faqs', function () {return view('FAQs.index');});
 // 404
 Route::get('/404', function () {return view('errors.404');});
 
